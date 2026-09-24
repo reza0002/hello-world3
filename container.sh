d61a19a79e974d25cac1b09ee838b688b61d47c6
reza0002 <remo0002@stud.ek.dk>
2026-09-24 09:59:17 +0200

    Add commands to stop and remove Docker containers

--- container.sh
+++ container.sh
@@ -4,6 +4,9 @@ docker run -d --name web -p 8080:80 nginx
 docker ps
 docker exec web ls /usr/share/nginx/html
 docker logs web
-
+docker stop web
+docker rm web
 
 docker run -d --name web2 nginx
+docker stop web2
+docker rm web2
